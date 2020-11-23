05bb61d99309767b038ce74649f1b1b71b46cfb7
Gianni Narracci <gnarracci@gmail.com>
2020-11-23 10:33:59 -0400

    Componets Loaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { NoMatch } from "./components/NoMatch";
+import { Layout } from "./components/Layout";
+import { NavigationBar } from "./components/NavigationBar";
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+      vulputate accumsan sapien eu accumsan. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Vestibulum aliquet tempus neque sed ornare.
+      Phasellus sagittis dignissim risus eu auctor. Quisque vestibulum leo eu
+      nunc sodales, rhoncus dapibus urna ultricies. Aenean lacinia velit
+      fermentum fringilla ornare. Sed sollicitudin ligula quis nisl ullamcorper,
+      eget vestibulum turpis facilisis. Cras et leo nec nibh dictum condimentum
+      ac nec mauris. Nullam tincidunt accumsan urna vitae pulvinar. Morbi varius
+      sem sit amet blandit congue. Praesent quis nunc vulputate dolor
+      ullamcorper tristique. Donec id nibh mauris. Duis gravida urna in lacus
+      tempor blandit. Proin elementum ultricies quam. Proin dapibus quis libero
+      ac vestibulum. Cras vestibulum tristique ligula, in condimentum leo
+      iaculis vitae. Nam placerat suscipit orci, molestie gravida mi pretium
+      nec. Nam viverra sit amet erat sed consectetur. Aenean pharetra mauris
+      eleifend elementum ullamcorper. Nunc molestie, erat a efficitur iaculis,
+      tellus dolor interdum tellus, ac pulvinar nulla neque sit amet leo. Duis
+      viverra, nisl ut dictum tempor, justo elit malesuada sem, sit amet
+      consequat lectus orci eu dolor. Nulla a magna felis. Nunc suscipit justo
+      tortor, nec interdum enim convallis ut. In quis libero aliquet tellus
+      posuere facilisis eu eget dui. Aliquam convallis arcu nibh, vitae
+      convallis ligula lobortis quis. Nam pharetra lorem in metus tincidunt
+      lobortis. Integer accumsan vulputate facilisis. Suspendisse commodo tempor
+      turpis vitae facilisis. Vivamus sed imperdiet augue. Ut euismod, est sed
+      ornare tincidunt, erat nisl vehicula ipsum, vitae facilisis dolor elit et
+      ante. Phasellus sapien libero, ullamcorper et molestie vel, suscipit ut
+      ex. Nullam dapibus, turpis et molestie luctus, tortor orci posuere quam,
+      in gravida diam orci non dui. Curabitur laoreet, lacus nec venenatis
+      dapibus, orci felis dictum lorem, quis convallis magna sapien non ante.
+      Quisque vel ex vitae purus dignissim feugiat ac ut dui. Pellentesque
+      convallis commodo feugiat. Duis porta vestibulum imperdiet. Phasellus
+      tempor neque est. Maecenas posuere, neque at tempus eleifend, massa odio
+      viverra ex, et tristique massa enim ultrices ante. Nulla dictum convallis
+      egestas. Sed diam nibh, mattis in orci sed, sodales lobortis felis. Aenean
+      rhoncus massa vitae erat aliquet sodales. Donec sollicitudin viverra nibh,
+      vitae sodales diam iaculis ut. Duis metus ipsum, luctus quis placerat at,
+      tempor nec magna. Nam neque mi, lacinia et sem vel, accumsan blandit
+      nulla. Morbi ornare dapibus urna, non vulputate lectus consequat quis.
+      Curabitur risus ipsum, venenatis non augue sit amet, mollis convallis
+      lacus. Ut nec facilisis nulla, eu sollicitudin ante. Praesent massa neque,
+      tristique quis sodales id, congue ut augue. Pellentesque magna orci,
+      scelerisque ut bibendum non, posuere a augue. Aenean ipsum nibh, eleifend
+      ut orci in, interdum imperdiet elit. Praesent justo enim, euismod et
+      sagittis ac, facilisis non neque. Nunc nulla odio, vestibulum vitae orci
+      eget, cursus blandit ante. Suspendisse elementum ornare fringilla.
+      Vestibulum posuere aliquet risus at facilisis. Suspendisse potenti.
+      Pellentesque consectetur nibh eget ex dapibus pulvinar. Praesent sit amet
+      scelerisque urna. Curabitur dignissim velit est, a iaculis leo faucibus
+      in. Donec nibh eros, suscipit in elit a, cursus ornare quam. Curabitur vel
+      ex enim.
+    </p>
+  </div>
+);
