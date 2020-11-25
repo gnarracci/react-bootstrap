2c0bba6f77ead4d098a38ce95d02f0f6195d2c1d
Gianni Narracci <gnarracci@gmail.com>
2020-11-25 03:33:03 -0400

    CCS Jumbo Modified

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -8,7 +8,7 @@ const Styles = styled.div`
     background: url(${imac}) no-repeat fixed bottom;
     background-size: cover;
     color: #000;
-    height: 250px;
+    height: 450px;
     position: relative;
     z-index: -2;
 
