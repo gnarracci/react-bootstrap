3ab16ce3fa35a574b9afc62bf033ffe2221312f6
Gianni Narracci <gnarracci@gmail.com>
2020-11-25 03:06:54 -0400

    Style Comp on Jumbotron Completed

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import {Jumbotron as Jumbo, Container} from 'react-bootstrap';
-import styled from 'styled-components';
-import boatimage from '../assets/boat.jpg';
+import React from "react";
+import { Jumbotron as Jumbo, Container } from "react-bootstrap";
+import styled from "styled-components";
+import imac from "../assets/imac.jpg";
 
 const Styles = styled.div`
+  .jumbotron {
+    background: url(${imac}) no-repeat fixed bottom;
+    background-size: cover;
+    color: #000;
+    height: 250px;
+    position: relative;
+    z-index: -2;
 
+    .overlay {
+      background-colr: #000;
+      opacity: 0.6;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      z-index: -1;
+    }
+  }
 `;
 
 export const Jumbotron = () => (
@@ -18,6 +36,4 @@ export const Jumbotron = () => (
       </div>
     </Jumbo>
   </Styles>
-)
-
-
+);
